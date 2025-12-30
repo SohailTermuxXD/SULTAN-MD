a231b66a7a7806e41458f687e13de72d4dea7f92
SohailTermuxXD <166821840+SohailTermuxXD@users.noreply.github.com>
2025-12-30 13:10:20 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,1 @@
+// Deploy From SohailTechXD repo not this *
---
+++ settings.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  SESSION_ID: "",  // add session Id 
+  
+  OWNER_NUMBER: "9231765508812", // add owner number 
+  
+  PREFIX: ".", // prefix (e.g., ., /, !, *)
+  
+  TIMEZONE: "Asia/Karachi" // put your country timeZone....leave blank if u don't know.
+};
+
