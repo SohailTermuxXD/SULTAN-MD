98d325bac52bfd1ce6bbde0e4455ac5f084da371
SohailTermuxXD <166821840+SohailTermuxXD@users.noreply.github.com>
2025-12-30 14:55:28 +0500

    Add SESSION_ID to settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "",  // add session Id 
+  SESSION_ID: "IK~6M8hxSqJ#RxCGyL9UOUakweURGevAoFgaYPIsYOPJ7xFkPMFwL6Q",  // add session Id 
   
   OWNER_NUMBER: "9231765508812", // add owner number 
   
